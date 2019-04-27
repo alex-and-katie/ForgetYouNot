796b6f72cd01428fb92557259a114cc02a7833e2
Alex Grazier <kylealexandra@gmail.com>
2019-04-27 12:32:54 -0400

    adds acquaintance model and associations

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -1,18 +1,10 @@
 const User = require('./user')
+const Acquaintance = require('./acquaintance')
 
-/**
- * If we had any associations to make, this would be a great place to put them!
- * ex. if we had another model called BlogPost, we might say:
- *
- *    BlogPost.belongsTo(User)
- */
+Acquaintance.belongsTo(User)
+User.hasMany(Acquaintance)
 
-/**
- * We'll export all of our models here, so that any time a module needs a model,
- * we can just require it from 'db/models'
- * for example, we can say: const {User} = require('../db/models')
- * instead of: const User = require('../db/models/user')
- */
 module.exports = {
-  User
+  User,
+  Acquaintance
 }
