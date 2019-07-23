243a221f210c6480a8a759d49408030797b27952
Alex Grazier <kylealexandra@gmail.com>
2019-07-23 17:41:21 -0400

    adds singleName reducer to combinedReducer

--- client/store/index.js
+++ client/store/index.js
@@ -5,8 +5,9 @@ import {composeWithDevTools} from 'redux-devtools-extension'
 import user from './user'
 import name from './name'
 import allNames from './allNames'
+import singleName from './singleName'
 
-const reducer = combineReducers({user, name, allNames})
+const reducer = combineReducers({user, name, allNames, singleName})
 const middleware = composeWithDevTools(
   applyMiddleware(thunkMiddleware, createLogger({collapsed: true}))
 )
@@ -15,3 +16,4 @@ const store = createStore(reducer, middleware)
 export default store
 export * from './user'
 export * from './name'
+export * from './singleName'
